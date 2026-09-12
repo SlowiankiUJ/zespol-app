4f532f6f5a4bc6894e2ad37ec2a2b6728eab4efe
Pawel <ujslowianki@gmail.com>
2026-09-12 12:02:00 +0200

    pow6

--- api/powiadomienie.js
+++ api/powiadomienie.js
@@ -10,11 +10,11 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Key os_v2_app_fbd76qqndrewrhsqur7ef7o2yuvi5cfyhwdunweezdincckgj5sxlxbqnlkqmgnlemdqhwndx7odus2ugcj6szq5ngjsadkb5ym53oa"
+        // Wklej tutaj swój nowy klucz po słowie "Key "
+        "Authorization": "Key TUTAJ_WKLEJ_SWÓJ_NOWY_KLUCZ"
       },
       body: JSON.stringify({
         app_id: "2847ff42-0d1c-4968-9e50-a47e42fddac5",
-        // Zmieniamy na segment "All", który obejmuje absolutnie wszystkie subskrypcje
         included_segments: ["All"],
         headings: { "en": tytul },
         contents: { "en": tresc }
@@ -22,7 +22,6 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    console.OdpowiedzOneSignal = data;
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
