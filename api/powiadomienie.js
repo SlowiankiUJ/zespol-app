8a60ed009a248df65ac15caa44798fa06bd73293
Pawel <ujslowianki@gmail.com>
2026-09-12 12:00:44 +0200

    pow5

--- api/powiadomienie.js
+++ api/powiadomienie.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Do niedozwolone' });
+    return res.status(405).json({ message: 'Metoda niedozwolona' });
   }
 
   const { tytul, tresc } = req.body;
@@ -10,18 +10,19 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        // Twój klucz jest teraz ukryty na zapleczu serwera!
         "Authorization": "Key os_v2_app_fbd76qqndrewrhsqur7ef7o2yuvi5cfyhwdunweezdincckgj5sxlxbqnlkqmgnlemdqhwndx7odus2ugcj6szq5ngjsadkb5ym53oa"
       },
       body: JSON.stringify({
         app_id: "2847ff42-0d1c-4968-9e50-a47e42fddac5",
-        included_segments: ["Total Subscriptions"],
+        // Zmieniamy na segment "All", który obejmuje absolutnie wszystkie subskrypcje
+        included_segments: ["All"],
         headings: { "en": tytul },
         contents: { "en": tresc }
       })
     });
 
     const data = await response.json();
+    console.OdpowiedzOneSignal = data;
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
