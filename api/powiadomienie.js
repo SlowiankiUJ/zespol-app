723ea129a74745ba2b7f6beb956432b20cd9d808
Pawel <ujslowianki@gmail.com>
2026-09-12 12:13:55 +0200

    push10

--- api/powiadomienie.js
+++ api/powiadomienie.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": "Basic os_v2_app_fbd76qqndrewrhsqur7ef7o2ywilikq745jeypvveiumysplzbcpdtr2lfffpxlc27po2cdvjutnz3parwxrn7c6ckykrgmkd4gv4oy"
+        "Authorization": "Key os_v2_app_fbd76qqndrewrhsqur7ef7o2ywilikq745jeypvveiumysplzbcpdtr2lfffpxlc27po2cdvjutnz3parwxrn7c6ckykrgmkd4gv4oy"
       },
       body: JSON.stringify({
         app_id: "2847ff42-0d1c-4968-9e50-a47e42fddac5",
