bf33e754417555d8c852ab2445f3de3d8cf78b89
Pawel <ujslowianki@gmail.com>
2026-09-12 12:03:43 +0200

    pow7

--- api/powiadomienie.js
+++ api/powiadomienie.js
@@ -10,8 +10,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        // Wklej tutaj swój nowy klucz po słowie "Key "
-        "Authorization": "Key TUTAJ_WKLEJ_SWÓJ_NOWY_KLUCZ"
+        "Authorization": "Key os_v2_app_fbd76qqndrewrhsqur7ef7o2yuvi5cfyhwdunweezdincckgj5sxlxbqnlkqmgnlemdqhwndx7odus2ugcj6szq5ngjsadkb5ym53oa"
       },
       body: JSON.stringify({
         app_id: "2847ff42-0d1c-4968-9e50-a47e42fddac5",
@@ -22,6 +21,10 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
+    
+    // WYPISUJEMY PEŁNĄ ODPOWIEDŹ W KONSOLI VERCELA
+    console.log("ODPOWIEDŹ ONESIGNAL:", JSON.stringify(data));
+
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
