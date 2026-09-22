afe585373c4f44daaddcc3b1c94b6a5792f430c7
Pawel <ujslowianki@gmail.com>
2026-09-23 00:32:27 +0200

    co jest

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://jwtidsgxtqvwihggpmuq.supabase.co'
-const supabaseAnonKey = 'sb_publishable_pDxODPx16DcbeQXut3l6dw_ZeqhXxLf'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3dGlkc2d4dHF2d2loZ2dwbXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxMzc2MTgsImV4cCI6MjEwNDcxMzYxOH0.-9KyYt5vp0nVdhVp1z_08_A2SibMSOvzJ1WIg_W6xEs'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
